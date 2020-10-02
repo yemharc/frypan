ee3169f66d0a0c48687e0aeab243aedae1e89090
yemharc <yemharc@gmail.com>
2020-10-02 22:35:32 +0900

    편의성 개선
    배포버전 문제점 개선
    - 기본 open 폴더 설정
    - 에러 해결

--- Frypan_Controllers/data_control.py
+++ Frypan_Controllers/data_control.py
@@ -9,18 +9,25 @@ class DataMgr:
     def __init__(self):
         self._ftypes = [("CSV","*.csv"), ("Excel",("*.xlsx","*.xls")),("Text","*.txt")]
         self._print_header = []
+        self.dest = os.getcwd() + '/output'
         
     def __del__(self):
         print("delete instance : ", __name__)
         
     def AddFiles(self):
-        return fdig.askopenfilenames(title="파일을 선택하세요", filetypes=self._ftypes, initialdir=r"c:/dev")
+        if not os.path.isdir(self.dest):
+            os.mkdir(self.dest)
+        return fdig.askopenfilenames(title="파일을 선택하세요", filetypes=self._ftypes, initialdir=self.dest)
     
     def AddFile(self):
-        return fdig.askopenfilename(title="파일을 선택하세요", filetype=self._ftypes, initialdir=r"c:/dev")
+        if not os.path.isdir(self.dest):
+            os.mkdir(self.dest)
+        return fdig.askopenfilename(title="파일을 선택하세요", filetype=self._ftypes, initialdir=self.dest)
 
     def GetDir(self):
-        return fdig.askdirectory(title="폴더를 선택하세요", initialdir=r"c:/dev")
+        if not os.path.isdir(self.dest):
+            os.mkdir(self.dest)
+        return fdig.askdirectory(title="폴더를 선택하세요", initialdir=self.dest)
     
     def Merge(self, df, lists):
         df = pd.DataFrame(data=None)
